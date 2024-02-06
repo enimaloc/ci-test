b1d022e6f0e8a60e7e1bf135f42ca7874cbcc91f
Antoine <antoine@enimaloc.fr>
2024-02-06 05:41:30 +0100

    f

--- src/main/java/fr/hashimiste/maps/Grille.java
+++ src/main/java/fr/hashimiste/maps/Grille.java
@@ -4,17 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 
- 
- 
- 
- 
+/**
+ * La classe Grille représente une grille dans un jeu ou une tâche.
+ * Elle contient des informations sur le niveau de difficulté de la grille, les îles (Ile) dans la grille et les ponts (Pont) dans la grille.
+ * Elle fournit des méthodes pour récupérer des informations sur la grille, ses îles et ses ponts.
+ */
 public class Grille {
     private final Object difficulte;
     private final List<Object> iles;
     private final List<Object> ponts;
 
     /**
-     * Dumb method
+     * Créer une grille
+     *
+     * @param difficulte la difficulté de la grille
+     * @param iles       les iles de la grille
+     * @param ponts      les ponts de la grille
      */
     public Grille(Object difficulte, List<Object> iles, List<Object> ponts) {
         this.difficulte = difficulte;
@@ -22,46 +27,47 @@ public class Grille {
         this.ponts = ponts;
     }
 
-
     /**
-     * Incorrect taggings
-     * @par difficulte
-     * @par iles
+     * Créer une grille
+     *
+     * @param difficulte la difficulté de la grille
+     * @param iles      les iles de la grille
      */
     public Grille(Object difficulte, List<Object> iles) {
         this(difficulte, iles, new ArrayList<>());
     }
 
-    
-     
-     
-
-    @SuppressWarnings("doclint")
+    /**
+     * Créer une grille
+     *
+     * @param difficulte la difficulté de la grille
+     */
     public Grille(Object difficulte) {
         this(difficulte, new ArrayList<>());
     }
 
-    
-     
-     
-     
-     @SuppressWarnings("all")
-    public Object getObject() {
+    /**
+     * Récupérer la difficulté de la grille
+     *
+     * @return la difficulté de la grille
+     */
+    public Object getDifficulte() {
         return difficulte;
     }
 
-    
-     
-     
-     // dump
-     
+    /**
+     * Récupérer les iles de la grille
+     *
+     * @return les iles de la grille
+     */
     public List<Object> getIles() {
         return iles;
     }
 
-
     /**
-     * @param should not be herre
+     * Récupérer les ponts de la grille
+     *
+     * @return les ponts de la grille
      */
     public List<Object> getPonts() {
         return ponts;
