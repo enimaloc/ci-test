01e0faff9072a273edc47ed7f407a71d6284515f
Antoine <antoine@enimaloc.fr>
2024-02-06 04:10:22 +0100

    f

--- src/main/java/fr/hashimiste/maps/Grille.java
+++ src/main/java/fr/hashimiste/maps/Grille.java
@@ -13,18 +13,21 @@ public class Grille {
     private final List<Object> iles;
     private final List<Object> ponts;
 
+    /**
+     * Dumb method
+     */
     public Grille(Object difficulte, List<Object> iles, List<Object> ponts) {
         this.difficulte = difficulte;
         this.iles = iles;
         this.ponts = ponts;
     }
 
-    
-     
-     
-     
-     
-     
+
+    /**
+     * Incorrect taggings
+     * @par difficulte
+     * @par iles
+     */
     public Grille(Object difficulte, List<Object> iles) {
         this(difficulte, iles, new ArrayList<>());
     }
